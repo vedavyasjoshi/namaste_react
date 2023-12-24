999d68d26cd8951606af8c693fd0c94b45cec606
Vedavyas <vedavyasjoshi121@gmail.com>
2023-12-24 11:21:12 +0530

    Intro to JSX

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  { id: "heading1" },
-  "Hello from React 1"
+const heading2 = (
+  <h1 className="heading" tabIndex={5}>
+    Hello from JSX 🚀
+  </h1>
 );
-const heading2 = React.createElement(
-    "h1",
-    { id: "heading2" },
-    "Hello from React 2"
-  );
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [heading1, heading2])
-);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading2);
