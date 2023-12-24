488bbde85b849bb504a294170465a680bf87529f
Vedavyas <vedavyasjoshi121@gmail.com>
2023-12-24 11:55:36 +0530

    Intro to functional components

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading2 = (
-  <h1 className="heading" tabIndex={5}>
-    Hello from JSX 🚀
-  </h1>
+const Title = function () {
+  return (
+    <h1 className="heading" tabIndex={5}>
+      Hello from JSX 🚀
+    </h1>
+  );
+};
+
+const HeadingComponent = () => <h1>Functional Component</h1>;
+const HeadingComponent2 = () => (
+  <h1 className="heading">Functional Component2</h1>
 );
 
+const number=1000;
+const HeadingComponent3 = () => {
+  return (
+    <div>
+      {number}
+      {Title()}
+      <Title />
+      <Title></Title>
+      {console.log("Hey there")}
+      <h1 className="heading">Functional Component3 {number+100}</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading2);
+root.render(<HeadingComponent3 />);
